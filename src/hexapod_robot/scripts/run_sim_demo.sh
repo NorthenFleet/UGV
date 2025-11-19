31c0b8a1b7a45f35a0119ecd4efcd8af12ef227d
“NorthenFleet” <violent-sinper@163.com>
2025-11-19 21:48:10 +0800

    重构

--- src/hexapod_robot/scripts/run_sim_demo.sh
+++ src/hexapod_robot/scripts/run_sim_demo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-export PYTHONPATH="$(pwd)"
+export PYTHONPATH="$(pwd)/src:$(pwd)"
 python -m hexapod_robot.sim.pybullet_env.scripts.visualize_hexapod
--- src/hexapod_robot/scripts/train_walk_ppo.sh
+++ src/hexapod_robot/scripts/train_walk_ppo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-export PYTHONPATH="$(pwd)"
+export PYTHONPATH="$(pwd)/src:$(pwd)"
 python training/train_walk_ppo.py
